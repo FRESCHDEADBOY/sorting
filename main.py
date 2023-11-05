fea3460199c73b6d4d80de6c1385f2de5ed8d9e0
freshdeadboy <zajchuk2014@gmaul.com>
2023-11-05 14:29:47 +0100

    update2

--- main.py
+++ main.py
@@ -30,19 +30,43 @@ def main(folder: Path):
         handle_media(file, folder / 'images' / 'PNG')
     for file in file_parser.SVG_IMAGES:
         handle_media(file, folder / 'images' / 'SVG')
-    for file in file_parser.MP3_AUDIO:
-        handle_media(file, folder / 'audio' / 'MP3_AUDIO')
+    for file in file_parser.AVI_VIDEO:
+        handle_media(file, folder / 'videos' / 'AVI')
+    for file in file_parser.MP4_VIDEO:
+        handle_media(file, folder / 'videos' / 'MP4')
+    for file in file_parser.MOV_VIDEO:
+        handle_media(file, folder / 'videos' / 'MOV')
+    for file in file_parser.MKV_VIDEO:
+        handle_media(file, folder / 'videos' / 'MKV')
+    for file in file_parser.DOC_DOCUMENTS:
+        handle_media(file, folder / 'documents' / 'DOC')
+    for file in file_parser.DOCX_DOCUMENTS:
+        handle_media(file, folder / 'documents' / 'DOCX')
+    for file in file_parser.TXT_DOCUMENTS:
+        handle_media(file, folder / 'documents' / 'TXT')
+    for file in file_parser.PDF_DOCUMENTS:
+        handle_media(file, folder / 'documents' / 'PDF')
+    for file in file_parser.XLSX_DOCUMENTS:
+        handle_media(file, folder / 'documents' / 'XLSX')
+    for file in file_parser.PPTX_DOCUMENTS:
+        handle_media(file, folder / 'documents' / 'PPTX')
+    for file in file_parser.MP3_MUSIC:
+        handle_media(file, folder / 'music' / 'MP3')
+    for file in file_parser.OGG_MUSIC:
+        handle_media(file, folder / 'music' / 'OGG')
+    for file in file_parser.WAV_MUSIC:
+        handle_media(file, folder / 'music' / 'WAV')
+    for file in file_parser.AMR_MUSIC:
+        handle_media(file, folder / 'music' / 'AMR')
+    for file in file_parser.ZIP_ARCHIVES:
+        handle_archive(file, folder / 'archives' / 'ZIP')
+    for file in file_parser.GZ_ARCHIVES:
+        handle_archive(file, folder / 'archives' / 'GZ')
+    for file in file_parser.TAR_ARCHIVES:
+        handle_archive(file, folder / 'archives' / 'TAR')
     for file in file_parser.MY_OTHER:
         handle_media(file, folder / 'MY_OTHER')
 
-    for file in file_parser.ARCHIVES:
-        handle_archive(file, folder / 'ARCHIVES')
-    for file in file_parser.DOCUMENTS:
-        handle_media(file, folder / 'documents')
-    for file in file_parser.VIDEO:
-        handle_media(file, folder / 'videos')
-    for file in file_parser.MUSIC:
-        handle_media(file, folder / 'music')
 
     for folder in file_parser.FOLDERS[::-1]:
         # Видаляємо пусті папки після сортування
